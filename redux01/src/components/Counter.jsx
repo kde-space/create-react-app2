52abbadd5cc326db77d03f1b759920275f664cc1
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-09-18 03:16:28 +0900

    React.SFC化

--- redux01/src/components/Counter.jsx
+++ redux01/src/components/Counter.jsx
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-    this.incrementAsync = this.incrementAsync.bind(this);
-  }
+const Counter = (props) => {
+  const { value, onIncrement, onDecrement } = props;
 
-  incrementIfOdd() {
-    if (this.props.value % 2 !== 0) {
-      this.props.onIncrement();
+  const incrementIfOdd = () => {
+    if (value % 2 !== 0) {
+      onIncrement();
     }
   }
 
-  incrementAsync() {
-    setTimeout(this.props.onIncrement, 1000);
+  const incrementAsync = () => {
+    setTimeout(onIncrement, 1000);
   }
 
-  render() {
-    const { value, onIncrement, onDecrement } = this.props;
-    return (
-      <p>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>+</button>
-        {' '}
-        <button onClick={onDecrement}>-</button>
-        {' '}
-        <button onClick={this.incrementIfOdd}>Increment if odd</button>
-        <button onClick={this.incrementAsync}>Increment async</button>
-      </p>
-    );
-  }
+  return (
+    <p>
+      Clicked: {value} times
+      {' '}
+      <button onClick={onIncrement}>+</button>
+      {' '}
+      <button onClick={onDecrement}>-</button>
+      {' '}
+      <button onClick={incrementIfOdd}>Increment if odd</button>
+      <button onClick={incrementAsync}>Increment async</button>
+    </p>
+  );
 }
 
 export default Counter;
