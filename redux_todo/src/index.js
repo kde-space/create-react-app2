d4b26d2a10f8eab817d8a30069c15834ffc3ce90
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-09-20 03:52:44 +0900

    reduxだけでの動作テスト

--- redux_todo/src/index.js
+++ redux_todo/src/index.js
@@ -1,6 +1,26 @@
 import { createStore } from 'redux';
+import todoApp from './reducers';
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './actions';
 
-// action
-const ADD_TODO = 'ADD_TODO';
-const TOGGLE_TODO = 'TOGGLE_TODO';
-const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+const store = createStore(todoApp);
+
+// 初期状態のログを取る
+console.log(store.getState());
+
+// 状態が変化するたびにログを取る
+// subscribe()はリスナーを登録解除するための関数を返す
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addTodo('起きる'));
+store.dispatch(addTodo('顔を洗う'));
+store.dispatch(addTodo('朝御飯を食べる'));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+unsubscribe();
+store.dispatch(addTodo('歯を磨く'));
+
