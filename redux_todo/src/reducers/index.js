5c8999a213d57baa6198cf12e11a240e9e778442
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-09-20 02:33:18 +0900

    reducerの分割

--- redux_todo/src/reducers/index.js
+++ redux_todo/src/reducers/index.js
@@ -1,47 +1,58 @@
+import { combineReducers } from 'redux';
+
 import { 
   ADD_TODO, 
   TOGGLE_TODO, 
   SET_VISIBILITY_FILTER, 
   VisibilityFilters 
 } from '../actions';
+const { SHOW_ALL } = VisibilityFilters;
 
-const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
-};
-
-function todoApp(state = initialState, action) {
+function visibilityFilter(state = SHOW_ALL, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
-      return {
-        ...state,
-        visibilityFilter: action.filter
-      };
-    case ADD_TODO:
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
-      };
-    case TOGGLE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return {
-              ...todo,
-              index: !todo.completed
-            };
-          }
-          return todo;
-        })
-      };
+      return action.filter
     default:
       return state;
   }
 }
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ];
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+});
+
+// 上記の同じ意味
+// function todoApp(state = {}, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   };
+// }
+
+export default todoApp;
