ebea601fe45eba6bcdcc1b1f39a4ebe4a984c61f
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-09-20 02:10:10 +0900

    基本reducer作成

--- redux_todo/src/reducers/index.js
+++ redux_todo/src/reducers/index.js
@@ -13,11 +13,35 @@ const initialState = {
 function todoApp(state = initialState, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
-     return {
-       ...state,
-       visibilityFilter: action.filter
-     };
+      return {
+        ...state,
+        visibilityFilter: action.filter
+      };
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return {
+              ...todo,
+              index: !todo.completed
+            };
+          }
+          return todo;
+        })
+      };
     default:
-     return state;
+      return state;
   }
 }
