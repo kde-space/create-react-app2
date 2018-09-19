404d26e166d0ff963d3cd93097568126e953d918
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-09-20 04:30:19 +0900

    プレゼンテーショナルコンポーネント 途中まで

---
+++ redux_todo/src/components/Todo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = ({ text, completed, onClick }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >
+    {text}
+  </li>
+);
+
+Todo.propTypes = {
+  text: PropTypes.string.isRequired,
+  completed: PropTypes.boolean.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default Todo;
