e6fd45984eb2f3965abf5eb31c95d5d46f97437f
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-06 02:42:02 +0900

    初めから

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoAppReducer from './reducers';
-import App from './components/App';
-
-const store = createStore(todoAppReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, 
+  <div>
+    Hello
+  </div>,
   document.getElementById('root')
 );
