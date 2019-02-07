6802e0748fb173f0922278747cdea56094e31c67
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-08 02:34:54 +0900

    初期化

--- async_app/src/index.js
+++ async_app/src/index.js
@@ -1,31 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { selectSubreddit, fetchPostsIfNeeded } from './actions';
-import rootReducer from './reducers';
+import { render } from 'react-dom';
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-  )
-);
-
-store.dispatch(selectSubreddit('reactjs'));
-store.dispatch(fetchPostsIfNeeded('reactjs'))
-  .then(() => console.log(store.getState()))
-  .then(() => {
-    store.dispatch(selectSubreddit('vuejs'));
-  })
-  .then(() => {
-    store.dispatch(fetchPostsIfNeeded('vuejs'))
-      .then(() => console.log(store.getState()));
-  }
-)
-
-ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
+render(<div>Hello</div>, document.getElementById('root'));
