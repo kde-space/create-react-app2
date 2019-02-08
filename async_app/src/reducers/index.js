6ffbcdfec7705eb5254cf401dd8e061d53858ce2
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-08 20:34:07 +0900

    reducers作成

---
+++ async_app/src/reducers/index.js
@@ -0,0 +1,67 @@
+import { combineReducer, combineReducers } from 'redux';
+import {
+  SELECT_SUBREDDIT,
+  INVALIDATE_SUBREDDIT,
+  REQUEST_POSTS,
+  RECEIVE_POSTS
+} from '../actions';
+
+function selectedSubreddit(state = 'reactjs', action) {
+  switch (action.type) {
+    case SELECT_SUBREDDIT:
+      return action.payload.subreddit;
+    default:
+      return state;
+  }
+}
+
+function posts(state = {
+  isFetching: false,
+  didInvalidate: false,
+  items: []
+}, action) {
+  switch (action.type) {
+    case INVALIDATE_SUBREDDIT:
+      return {
+        ...state,
+        didInvalidate: true
+      };
+    case REQUEST_POSTS:
+      return {
+        ...state,
+        isFetching: true,
+        didInvalidate: false
+      };
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        isFetching: false,
+        didInvalidate: false,
+        items: action.payload.posts,
+        lastUpdated: action.payload.receiveAt
+      };
+    default:
+      return state;
+  }
+}
+
+function postsBySubreddit(state = {}, action) {
+  switch (action.type) {
+    case INVALIDATE_SUBREDDIT:
+    case REQUEST_POSTS:
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        [action.payload.subreddit]: posts(state[action.payload.subreddit], action)
+      };
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers(
+  selectedSubreddit,
+  postsBySubreddit
+);
+
+export default rootReducer;
